defd55a5b117f4be90f903a266dc6c29bac63a42
iirvine <icirvine@gmail.com>
2012-01-29 22:38:36 -0500

    first bit of work around verifying command line args

--- pset2/caesar.c
+++ pset2/caesar.c
@@ -1,11 +1,39 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 //cipher formula: c(i) = (p(i)+k)%26
-
+    //have to verify command line argument
+        //argv[1] must be non negative int
+        //argc must == 1
+    //have to convert argument to number...
+        //int i = atoi(someString);
 int
-main(int argc, string argv[])
+main(int argc, char *argv[])
 {
-    //herp derp here's some code
+    //We want a single argument (two counting the program name)
+    if (argc != 2)
+    {
+        printf("Invalid number of arguments! Just give me one number, bra.\n");
+        return 1;
+    }
+    
+    //Check that argument is an int...
+    int argLength = strlen(argv[1]); 
+    for (int i = 0; i < argLength; i++){
+        if (!isdigit(argv[1][i])){
+            printf("Character %d is not a digit! What're you tryin to pull....\n", i + 1);
+            return 2;
+        }
+    }
+    
+    int key = atoi(argv[1]);
+    
+    printf("The key is... %d\n", key);
+        
+    for (int i = 0; i < argc; i++)
+        printf("%s\n", argv[i]);
 
 }
