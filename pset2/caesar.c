b89e1182352bfba08f96d9bfd850e65d31f614c5
iirvine <icirvine@gmail.com>
2012-01-29 22:50:11 -0500

    implemented VerifyArgs function

--- pset2/caesar.c
+++ pset2/caesar.c
@@ -5,28 +5,15 @@
 #include <string.h>
 
 //cipher formula: c(i) = (p(i)+k)%26
-    //have to verify command line argument
-        //argv[1] must be non negative int
-        //argc must == 1
-    //have to convert argument to number...
-        //int i = atoi(someString);
+        
+bool VerifyArgs(int argc, char *argv[]);
+
 int
 main(int argc, char *argv[])
-{
-    //We want a single argument (two counting the program name)
-    if (argc != 2)
-    {
-        printf("Invalid number of arguments! Just give me one number, bra.\n");
+{    
+    if (!VerifyArgs(argc, argv)){
+        printf("Sumfin bad happen.\n");
         return 1;
-    }
-    
-    //Check that argument is an int...
-    int argLength = strlen(argv[1]); 
-    for (int i = 0; i < argLength; i++){
-        if (!isdigit(argv[1][i])){
-            printf("Character %d is not a digit! What're you tryin to pull....\n", i + 1);
-            return 2;
-        }
     }
     
     int key = atoi(argv[1]);
@@ -36,4 +23,25 @@ main(int argc, char *argv[])
     for (int i = 0; i < argc; i++)
         printf("%s\n", argv[i]);
 
+}
+
+bool
+VerifyArgs(int argc, char *argv[])
+{
+    //We want a single argument (two counting the program name)
+    if (argc != 2)
+    {
+        printf("Invalid number of arguments! Just give me one number, bra.\n");
+        return false;
+    }
+    
+    //Check that argument is an int...
+    int argLength = strlen(argv[1]); 
+    for (int i = 0; i < argLength; i++){
+        if (!isdigit(argv[1][i])){
+            printf("Character %d is not a digit! What're you tryin to pull....\n", i + 1);
+            return false;
+        }
+    }
+    return true;
 }
